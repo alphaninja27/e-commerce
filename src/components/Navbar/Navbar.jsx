2d860cbfce479f8d2dea2ba1fc36817de0347b96
alphaninja27 <72308743+alphaninja27@users.noreply.github.com>
2022-06-18 22:25:30 +0530

    edits

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const PrimarySearchAppBar = ({ totalItems }) => {
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> Website Name
+            <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> ReipEAT GUD
           </Typography>
           <Typography component={Link} to="/products" variant="h6" className={classes.title} color="inherit">
             Products 
